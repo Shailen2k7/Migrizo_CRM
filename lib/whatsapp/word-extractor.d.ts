8ae3616649574a159762def64520641ab4bafdaf
Shailen2k7 <shailenpathak@gmail.com>
2026-08-29 17:34:56 +0530

    Delete lib/whatsapp directory

--- lib/whatsapp/word-extractor.d.ts
+++
@@ -1,9 +0,0 @@
-// word-extractor ships no types. We use exactly this much of it.
-declare module 'word-extractor' {
-  class Document {
-    getBody(): string;
-  }
-  export default class WordExtractor {
-    extract(input: Buffer | string): Promise<Document>;
-  }
-}
